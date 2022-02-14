9b9886ee6fa0837d98d771fd67eb67738e7200f6
Didac Lopez <lopezm.didac@gmail.com>
2022-02-14 16:27:50 +0100

    Feature/ios (#28)
    * code smell fixing
    
    * add ios factory
    
    * update property
    
    * filter test cases
    
    * fix yml typo
    
    * add ios execution
    
    * fix yml test filters
    
    * change tags
    
    * disable android validation
    
    * add udid
    
    * update json
    
    * include platformversion at config
    
    * add wda config
    
    * install new WDA
    
    * timeout increase
    
    * paralel ios and android
    
    * archive naming appium evidences
    
    * remove obsolete filters
    
    * testresultpath
    
    * filter ios
    
    * resultsfile path
    
    * json config
    
    * merge push
    
    * evidences path
    
    * change error message

---
+++ src/Engines/TestWare.Engines.Appium/Factory/IOSDriver.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Service;
+
+namespace TestWare.Engines.Appium.Factory;
+
+public class IOSDriver : OpenQA.Selenium.Appium.iOS.IOSDriver, IAppiumDriver
+{
+    public IOSDriver(DriverOptions driverOptions) : base(driverOptions)
+    {
+    }
+
+    public IOSDriver(ICommandExecutor commandExecutor, DriverOptions driverOptions) : base(commandExecutor, driverOptions)
+    {
+    }
+
+    public IOSDriver(DriverOptions driverOptions, TimeSpan commandTimeout) : base(driverOptions, commandTimeout)
+    {
+    }
+
+    public IOSDriver(AppiumServiceBuilder builder, DriverOptions driverOptions) : base(builder, driverOptions)
+    {
+    }
+
+    public IOSDriver(Uri remoteAddress, DriverOptions driverOptions) : base(remoteAddress, driverOptions)
+    {
+    }
+
+    public IOSDriver(AppiumLocalService service, DriverOptions driverOptions) : base(service, driverOptions)
+    {
+    }
+
+    public IOSDriver(AppiumServiceBuilder builder, DriverOptions driverOptions, TimeSpan commandTimeout) : base(builder, driverOptions, commandTimeout)
+    {
+    }
+
+    public IOSDriver(Uri remoteAddress, DriverOptions driverOptions, TimeSpan commandTimeout) : base(remoteAddress, driverOptions, commandTimeout)
+    {
+    }
+
+    public IOSDriver(AppiumLocalService service, DriverOptions driverOptions, TimeSpan commandTimeout) : base(service, driverOptions, commandTimeout)
+    {
+    }
+}
